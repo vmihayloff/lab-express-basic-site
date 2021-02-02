d89ddb8484f2afba5425daa9b63f3b98f9e6b10a
Viktoria <viktoria.mihaylova92@gmail.com>
2021-02-02 19:18:43 +0100

    Style and photo added

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-const express = require ('express');
-const app = express ()
+const express = require("express");
+const app = express();
 app.use(express.static(__dirname + "/public"));
 
-app.get('/home', (req, res) => {
-    res.sendFile(__dirname + "/views/home.html")
-})
-app.get('/about', (req, res) => {
-    res.sendFile(__dirname + "/views/about.html")
-})
-app.get('/works', (req, res) => {
-    res.sendFile(__dirname + "/views/works.html")
-})
-app.get('/photoGallery', (req, res) => {
-    res.sendFile(__dirname + "/views/photoGallery.html")
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/home.html");
+});
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/views/about.html");
+});
+app.get("/works", (req, res) => {
+  res.sendFile(__dirname + "/views/works.html");
+});
+app.get("/photoGallery", (req, res) => {
+  res.sendFile(__dirname + "/views/photoGallery.html");
+});
 
-app.listen(3000)
-
+app.listen(3000);
