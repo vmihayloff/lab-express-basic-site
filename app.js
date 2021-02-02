eee929e276685bbea0337f13ee632250f935b6fc
Viktoria <viktoria.mihaylova92@gmail.com>
2021-02-02 17:46:35 +0100

    Solved lab

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require ('express');
+const app = express ()
+app.use(express.static(__dirname + "/public"));
+
+app.get('/home', (req, res) => {
+    res.sendFile(__dirname + "/views/home.html")
+})
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + "/views/about.html")
+})
+app.get('/works', (req, res) => {
+    res.sendFile(__dirname + "/views/works.html")
+})
+app.get('/photoGallery', (req, res) => {
+    res.sendFile(__dirname + "/views/photoGallery.html")
+})
+
+app.listen(3000)
+
